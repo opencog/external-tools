8079746df398f8979e5aa77240f522ac16422cb1
Samuel <sambod50@gmail.com>
2014-02-05 14:31:00 +0300

    table view for Atoms

--- AtomViewer/WebContent/lib/atomviewer/views/av_views.js
+++ AtomViewer/WebContent/lib/atomviewer/views/av_views.js
@@ -53,13 +53,146 @@ function updateTableView()
     if (av.atom_data != null)
     {
         //TODO
-        av.DOM.byId(idTableTab).innerHTML = "<h1 align='center'>(W.I.P. - not yet inplemented)</h1>";
+        av.DOM.byId(idTableTab).innerHTML =AtomsTableView(av.atom_data); //"<h1 align='center'>(W.I.P. - not yet inplemented)</h1>";
     }
     else
     {
         av.DOM.byId(idTableTab).innerHTML = "<h1 align='center'>(No Data)</h1>";
     }
 }
+
+//outerMost table that hase all tables inside. 
+function AtomsTableView(atoms)
+{
+  var table= "<table class='atomTable'>";
+  table+="<tr><th class='atomTableHeader'> total </th><th class='atomTableHeader' style='text-align:left;'>"+ atoms.length.toString()+"</th></tr>";	
+  table+="<tr><td class='cellId' style='vertical-align:text-top;' > atoms </td><td class='cellId'>"+upperTables(atoms)+"</td></tr></table>";
+  return table;
+}
+
+
+//upper table that actual content consists. 
+ 
+ function upperTables(atoms)
+ 	{    
+    var entr=true;
+ 	var tbody="";
+ 	var Table="<table  class='atomTable'> ";
+ 	    Table+="<tr><td class='cellId'>[Key]</td>";
+ 	 
+ 	for(var atom in atoms)
+ 	{
+ 	if(entr==true)
+ 	{
+ 	  Table+=CreateTableHeader(atoms[atom]);
+ 	}
+ 	 entr=false;
+ 	tbody +=  "<tr><td class='cellId'>"+atom+"</td>"+ innerTables(atoms[atom]); 	        
+ 	}
+ 	Table+=tbody+"</table>";
+ 	return Table;
+ 	}
+ 	
+ 	function CreateTableHeader(Columns_data)
+ 	{ 		
+ 		var thValue="";
+ 		for(var Column_data in Columns_data)
+ 		{
+ 			thValue+="<TH class='atomTableHeader'> "+ Column_data +" </TH>";
+ 		}
+ 		thValue+="</tr>";
+ 		return thValue;
+ 	}
+ 	
+ 	//Creates table for nested objects.	
+ 	function innerTables(atom)
+ 	{
+ 		var reval="";
+ 		var tbody="";
+ 		var tmp=null;
+ 		for(var i in atom)
+ 		{ 			 
+ 		   tmp=atom[i];
+ 			if(tmp=="[object Object]")
+ 			{ 
+ 			 var objStr="[-] Object, "+objectCount(tmp) +" Properties<br/><br/>";
+ 			 tmp=objStr+innerMostTable(tmp);
+ 			} 	
+ 			if(Object.prototype.toString.apply(atom[i])=="[object Array]")
+ 			{
+ 				if(atom[i]=="")
+ 				{
+ 					
+ 					tmp="[empty Array]";
+ 				}else
+ 				{
+ 					tmp="[-] Array, "+atom[i].length.toString()+" Item<br/><br/>";
+ 					tmp+=ArrayTable(atom[i]);
+ 				} 				 
+ 			}	    
+ 		   reval+=createTr(tmp); 		    
+ 		} 		 
+ 		 tbody+=reval;
+ 		return tbody;
+ 	}	
+ 	
+ 	//Creates a single table cell at atime.
+ 	function createTr(columns)
+ 	{ 	       
+ 		return "<td class='cellId'>"+columns+"</td>";
+ 	}
+ 	
+ 	 /**
+ 	  * creates table of arrays. array table and object table are in seperate 
+ 	  * function due to the nested nature of objects. 
+ 	  */
+ 	
+ 	function ArrayTable(array_data)
+ 	{
+ 		var aTable="<table class='atomTable'>";
+ 		var aBody="";
+ 		for(var aData in array_data)
+ 		{
+ 			aBody+="<tr><td class='cellId'>"+aData+"</td><td class='cellId'>"+array_data[aData] +"</td></tr>";
+ 		}
+ 		 aTable+=aBody+ "</table>";
+ 		return aTable;
+ 	}
+ 	
+   // counts the number of objects 
+   function objectCount(objects)
+   {
+   	var objCount=0;
+   	for( var obj in objects)
+   	{
+   		objCount++;
+   	}
+   	return objCount;
+   }
+ /**
+  * creates inner tables recursively 
+  */
+ 	function innerMostTable(atom)
+ 	{
+ 		var Innertable="<table class='atomTable' >";
+ 		var ibody="";
+ 		var tmp=null;
+ 		for(var i in atom)
+ 		{    tmp=atom[i];
+ 			if(tmp=="[object Object]")
+ 			{
+ 				 var objStr="[-] Object, "+objectCount(tmp) +" Properties<br/><br/>";
+                 tmp=objStr+innerMostTable(tmp); 	
+               		
+  			}
+ 			
+ 			ibody+="<tr><td class='cellId'>"+i+"</td><td class='cellId'>"+tmp+"</td></tr>";
+ 			
+ 		}
+ 		Innertable+=ibody+"</table> ";
+ 		return Innertable;
+ 	}
+ 	
 
 /*
  * Called to create/refresh the scheme view.
