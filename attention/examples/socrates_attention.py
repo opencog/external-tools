38887c6d25d73d7af47cd1443e676cda18140dce
Sebastian Ruder <ruder.sebastian@gmail.com>
2014-06-17 15:09:56 +0200

    Renamed socrates example

--- attention/examples/socrates_attention.py
+++ attention/examples/socrates_attention.py
@@ -7,7 +7,7 @@ __author__ = 'Sebastian Ruder'
 from attention.attention_interface import *
 from time import sleep
 
-socrates_agent = "../opencog/python/pln/examples/attentionallocation/socrates_agent"
+path = "../opencog/python/pln/examples/attentionallocation/socrates_agent"
 
 # Configurable parameters:
 num_steps = 10                            # Number of time steps
@@ -17,6 +17,7 @@ clear_atomspace()
 
 load_scheme_files(["python/pln/examples/relex2logic/socrates-r2l.scm"])
 
+
 timeseries = []
 
 # Run 'num_steps' cycles. At each discrete timestep, capture the STI values of
@@ -24,16 +25,17 @@ timeseries = []
 # attentional focus. When finished, exports the STI timeseries dataset to
 # 'output_filename'.
 
-load_python_agent(socrates_agent)
-
-for i in range(50):
-    step_agent(socrates_agent, "SocratesAgent")
-    sleep(.1)
-    print(i)
-
+load_python_agent(path)
 
 print(dump_atomspace_scheme())
 
+for i in range(50):
+    step_agent(path, "SocratesAgent")
+    sleep(.1)
+    print(i)
+
+print(dump_atomspace_scheme())
+"""
 for t in range(0, num_steps):
     point_in_time = get_attentional_focus(timestep=t, scheme=True)
     timeseries.append(point_in_time)
@@ -43,3 +45,4 @@ for t in range(0, num_steps):
           "{0}\n".format(point_in_time))
 
     sleep(.1)
+"""
