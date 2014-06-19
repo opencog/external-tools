db10d7ff52ef4f8e075dcbbb1850d3150bd0e23c
Sebastian Ruder <ruder.sebastian@gmail.com>
2014-06-19 09:19:39 +0200

    Updated socrates example

--- attention/examples/socrates_attention.py
+++ attention/examples/socrates_attention.py
@@ -7,42 +7,36 @@ __author__ = 'Sebastian Ruder'
 from attention.attention_interface import *
 from time import sleep
 
-path = "../opencog/python/pln/examples/attentionallocation/socrates_agent"
-
 # Configurable parameters:
-num_steps = 10                            # Number of time steps
+num_steps = 50                            # Number of time steps
 output_filename = 'ecan-timeseries.csv'   # Output filename
 
+path = "../opencog/python/pln/examples/attentionallocation/socrates_agent"
+name = "SocratesAgent"
+
 clear_atomspace()
+set_af_boundary(5)
+
+print(dump_atomspace_scheme())
+
+load_python_agent(path)
+start_python_agent(path, name)
 
 load_scheme_files(["python/pln/examples/relex2logic/socrates-r2l.scm"])
 
+# no query to be defined
 
 timeseries = []
 
-# Run 'num_steps' cycles. At each discrete timestep, capture the STI values of
-# the atoms in the attentional focus, and also capture a Scheme dump of the
-# attentional focus. When finished, exports the STI timeseries dataset to
-# 'output_filename'.
-
-load_python_agent(path)
-
-print(dump_atomspace_scheme())
-
-for i in range(50):
-    step_agent(path, "SocratesAgent")
-    sleep(.1)
-    print(i)
-
-print(dump_atomspace_scheme())
-"""
 for t in range(0, num_steps):
     point_in_time = get_attentional_focus(timestep=t, scheme=True)
     timeseries.append(point_in_time)
+
     importance_diffusion()
+    importance_updating()
+    step_python_agent(path, name)
 
-    print("------------------------------------------------------\n"
-          "{0}\n".format(point_in_time))
+    print "------------------------------------------------------\n"\
+          "{0}\n".format(point_in_time)
 
-    sleep(.1)
-"""
+export_timeseries_csv(timeseries, output_filename, scheme=True)
