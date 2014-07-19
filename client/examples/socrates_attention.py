fa0ee99b3097a8d00a01ebf01192236b7d2766b9
Sebastian Ruder <ruder.sebastian@gmail.com>
2014-07-19 17:57:00 +0200

    Added way to save and compare atomspaces in Python

--- client/examples/socrates_attention.py
+++ client/examples/socrates_attention.py
@@ -1,15 +1,19 @@
 """
-An adaptation of the interface defined in attention_interface.py to the Socrates demo
+An adaptation of the interface defined in client.py to the Socrates demo
 """
 
 __author__ = 'Sebastian Ruder'
 
 from client.client import *
-from opencog.atomspace import types
-from time import sleep
+from opencog.atomspace import AtomSpace, types
+from opencog.scheme_wrapper import __init__, load_scm
+
+data = ["opencog/atomspace/core_types.scm",
+        "opencog/scm/utilities.scm",
+        "opencog/python/pln/examples/attentionallocation/atomspace.log"]
 
 # Configurable parameters:
-num_steps = 50                            # Number of time steps
+num_steps = 2                            # Number of time steps
 output_filename = 'ecan-timeseries.csv'   # Output filename
 
 path = "../opencog/python/pln/examples/attentionallocation/" \
@@ -29,11 +33,22 @@ load_scheme_files(["python/pln/examples/attentionallocation" +
 
 # no query to be defined
 
-timeseries = []
+timeseries = []  # list of timesteps
+atomspace_series = []  # list of atomspaces pertaining to each timestep
+
+atomspace = AtomSpace()
+__init__(atomspace)
 
 for t in range(0, num_steps):
     point_in_time = get_attentional_focus(timestep=t, scheme=True)
     timeseries.append(point_in_time)
+
+    with open("../../../opencog/opencog/python/pln/examples/attentionallocation/atomspace.log", "w") as log:
+        log.write(dump_atomspace_scheme())
+    for item in data:
+        load_scm(atomspace, item)
+
+    atomspace_series.append(atomspace)
 
     importance_diffusion()
     importance_updating()
@@ -44,5 +59,21 @@ for t in range(0, num_steps):
 
     #print(dump_atomspace_scheme())
     print(dump_attentional_focus_scheme())
+
+current_atoms = []
+timestep_count = 0
+for atomspace in atomspace_series:
+
+    atoms = [atom for atom in atomspace.get_atoms_by_type(types.Atom)
+             if atom not in current_atoms
+             and not atom.type in (types.ConceptNode, types.PredicateNode,
+                                   types.VariableNode, types.ListLink)]
+    current_atoms.append(atoms)
+
+    print("Timestep: {0}; additional atoms:".format(timestep_count))
+    for atom in atoms:
+        print(atom)
+    print("\n")
+    timestep_count += 1
 
 export_timeseries_csv(timeseries, output_filename, scheme=True)
