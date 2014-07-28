57445dd8a5d3dfecbab4a775057a71d8ad026ba2
Sebastian Ruder <ruder.sebastian@gmail.com>
2014-07-28 12:46:05 +0200

    Implemented visualization of socrates demo

--- attention/examples/socrates_demo.py
+++ attention/examples/socrates_demo.py
@@ -1,17 +1,27 @@
 """
-For running socrates_agent.py without the cogserver
-Requires mongodb to be installed as specified in attention/README.md
-mongodb needs to be started with 'sudo service mongod start'
+A demo application to run socrates.py using Python using the visualization
+component but neglecting attention allocation (for the moment)
+
+Prerequisites:
+    - Requires mongodb to be installed as specified in attention/README.md
+    - mongodb needs to be started with 'sudo service mongod start'
+    - Requires a running CogServer to retrieve the dot representation after
+      each inference
+    - Requires a running REST API (to be started with 'restapi.Start' in the
+      CogServer)
+
+After the inference, the produced images can be viewed with
+'gwenview ./timeseries -s'.
 """
 
 import os
 from pln.examples.socrates_demo import socrates_agent
 from opencog.atomspace import types, AtomSpace, TruthValue
 from opencog.scheme_wrapper import load_scm, scheme_eval, scheme_eval_h, __init__
-from attentional_focus_slideshow import render_image
 from attention_interface import clear_atomspace, scheme, dump_atomspace_dot
+from subprocess import check_call
 
-__author__ = 'Cosmo Harrigan, Sebastian Ruder'
+__author__ = 'Sebastian Ruder'
 
 ANALYSIS_FOLDER = os.path.dirname(__file__)
 
@@ -19,30 +29,59 @@ sub_dir = "timeseries"
 if not os.path.exists(sub_dir):
     os.makedirs(sub_dir)
 
-# client = pymongo.MongoClient(MONGODB_CONNECTION_STRING)
-# mongo = client[MONGODB_DATABASE]
-#
-# points = mongo['points']
 
+#TODO: method should be ported to attention_interface.py so it can be reused
+#more easily
+def render_image(dot, uid):
+    """
+    Renders a PNG image from a DOT graph description;
+    Method copied from attentional_focus_slideshow.py because import didn't
+    work properly
+
+    Parameters:
+
+    dot (required) The DOT graph description of the point in time
+    uid (required) A unique identifier, which should increment at each time interval
+    """
+    dot_filename = "{0}-{1:05d}.dot".format(sub_dir, uid)
+    png_filename = "{0}-{1:05d}.png".format(sub_dir, uid)
+
+    dot_full_path = os.path.join(ANALYSIS_FOLDER, sub_dir, dot_filename)
+    png_full_path = os.path.join(ANALYSIS_FOLDER, sub_dir, png_filename)
+
+    with open(dot_full_path, 'w') as outfile:
+        outfile.write(dot)
+
+    # Render the image using Graphviz
+    check_call(['dot', '-Tpng', dot_full_path, '-o', png_full_path])
+    os.remove(dot_full_path)
+    print("\n-- Rendered image {0} --".format(png_filename))
+
+# Parameters
+num_steps = 1000
+print_starting_contents = True
+
+# Initialization of atomspace and agent
 atomspace = AtomSpace()
 __init__(atomspace)
+agent = socrates_agent.SocratesAgent()
 
+# Preloaded files
 coreTypes = "../opencog/atomspace/core_types.scm"
 utilities = "../opencog/scm/utilities.scm"
 data = "../opencog/python/pln/examples/socrates_demo/socrates-r2l.scm"
 
-num_steps = 1000
-print_starting_contents = True
-
 for item in [coreTypes, utilities, data]:
     load_scm(atomspace, item)
-
-agent = socrates_agent.SocratesAgent()
 
 if print_starting_contents:
     print('AtomSpace starting contents:')
     atomspace.print_list()
 
+# Types of links that should not be visualized
+not_visualized_links = [types.ExecutionLink, types.GroundedSchemaNode,
+                        types.ListLink, types.VariableNode]
+result_found = False
 outputs_produced = 0
 
 for i in range(0, num_steps):
@@ -52,6 +91,7 @@ for i in range(0, num_steps):
     input = None
     rule = None
     if result is not None:
+        atomspace_string = ""
         (rule, input, output) = result
         outputs_produced += 1
 
@@ -62,7 +102,35 @@ for i in range(0, num_steps):
         print("\n-- based on this input:\n{0}".format(input))
 
         clear_atomspace()
-        scheme(atomspace.get_atoms_by_type(types.Atom))
-        dot = dump_atomspace_dot()
+        # visualization filtering
+        for atom in atomspace.get_atoms_by_type(types.Atom):
+            # links that shouldn't be visualized are skipped
+            if atom.type in not_visualized_links:
+                continue
 
-        render_image(dot, outputs_produced, ANALYSIS_FOLDER, sub_dir)
+            if atom not in [types.ConceptNode, types.PredicateNode]:
+                out = [atom1 for atom1 in atomspace.get_outgoing(atom.h)]
+                # links involving automatically generated VariableNodes are
+                # skipped
+                if out and "$pln_var_" in out[0].name:
+                    continue
+                # check if result is found
+                if atom.is_a(types.EvaluationLink):
+                    if out[0].is_a(types.PredicateNode)\
+                        and "breathe" in out[0].name\
+                        and out[1].is_a(types.ListLink)\
+                        and "Socrates" in out[1][0].name\
+                        and "air" in out[1][1].name:
+                        result_found = True
+
+            atomspace_string += str(atom)
+
+        # The scheme representation is imported in the atomspace, the dot
+        # representation is retrieved and the image is rendered
+        scheme(atomspace_string)
+        dot = dump_atomspace_dot()
+        render_image(dot, outputs_produced)
+
+        if result_found:
+            print("\n-- Result found! --")
+            break
