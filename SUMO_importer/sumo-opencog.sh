bc89933c4471041852e807b27807181dad9d3d90
leungmanhin <manhinleung@gmail.com>
2017-03-17 18:34:50 +0800

    Move output out of sumo dir

--- SUMO_importer/sumo-opencog.sh
+++ SUMO_importer/sumo-opencog.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ ! -d "sumo" ]; then
-echo -e "Downloading sumo \n"
-git clone https://github.com/ontologyportal/sumo.git
-echo -e "Finished downloading SUMO kif files \n"
+    echo -e "Downloading sumo"
+    git clone https://github.com/ontologyportal/sumo.git
+    echo -e "Finished downloading SUMO kif files"
 fi
 
 echo -e " \n *** Creating scheme files *** \n"
@@ -13,12 +13,13 @@ for file in *.kif; do
    python ../sumo-importer.py $file;
 done
 
+cd ..
 echo -e " \n *** Coping the output files to /outputs *** \n "
 if [ ! -d "output" ]; then
     mkdir -p "output"
 fi
 
-for file in *.scm; do
+for file in sumo/*.scm; do
    mv $file output
 done
 
