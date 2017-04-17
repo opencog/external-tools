ad4917de25388c1bd5e5cdacf32d5c67d007a796
Nil Geisweiller <ngeiswei@gmail.com>
2017-04-17 10:05:34 +0300

    Move kif parser code in kifparser.py
    + implement rudimentary sumo to atom type inference code in
      sumo-instance-types.py
    
    + improve consistency of the sumo importer, like handle variable list
      correctly, have instance, attribute be replaced by MemberLink
      because that it more consistent with SUMO conventions, and more.

--- SUMO_importer/sumo-opencog.sh
+++ SUMO_importer/sumo-opencog.sh
@@ -1,27 +1,65 @@
 #!/bin/bash
 
+#############
+# Constants #
+#############
+
+INSTANCE_TYPE_FILE="instances.type"
+
+#############
+# Functions #
+#############
+
+# Info level logging
+info_echo() {
+    echo "[INFO] $@"
+}
+
+# Warning level logging on stderr
+warn_echo() {
+    echo "[WARN] $@" 1>&2
+}
+
+# Error level logging on stderr and exit
+error_echo() {
+    echo "[ERROR] $@" 1>&2
+    exit 1
+}
+
+########
+# Main #
+########
+
 if [ ! -d "sumo" ]; then
-echo -e "Downloading sumo \n"
+info_echo "Download sumo"
 git clone https://github.com/ontologyportal/sumo.git
-echo -e "Finished downloading SUMO kif files \n"
+info_echo "Finished downloading SUMO kif files"
 fi
 
-echo -e " \n *** Creating scheme files *** \n"
 cd "sumo/"
 
+if [ ! -f "$INSTANCE_TYPE_FILE" ]; then
+    info_echo "Infer atom types of SUMO instances, put results in sumo/$INSTANCE_TYPE_FILE"
+    info_echo "Be patient, it may take a while..."
+    ../sumo-instance-types.py *.kif >> "$INSTANCE_TYPE_FILE"
+fi
+
+info_echo "Create scheme files"
+
 for file in *.kif; do
-   python ../sumo-importer.py $file; 
+    info_echo "Export $file"
+    ../sumo-importer.py "$INSTANCE_TYPE_FILE" $file
 done
 
-echo -e " \n *** Coping the output files to /outputs *** \n "
+info_echo "Move the generated scheme files to sumo/output"
 if [ ! -d "output" ]; then
     mkdir -p "output"
 fi
 
-for file in *.scm; do
-   mv $file output   
-done
+mv *.scm output
 
-echo "Done"
+cd ..
+
+info_echo "Done"
 
 
