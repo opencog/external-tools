17e8eea95244b271016acd3e36f1183f49e08a1b
sshermz <sshermz00-github@yahoo.com>
2017-10-17 12:17:30 -0700

    Package updates (Angular 4, etc), plus minor feature & bug fixes.
    - Updated Angular 2 to Angular 4, and other packages to latest where ever possible. Fixes as necessary due to deprecations, including to MaterialModule. Also some TSLint fixes. Fixes #97.
    - New Feature: Keep in paused mode if drag Node while paused (Simulation will tick while dragging, but will now repause when the drag is completed).
    - Bug Fix: Revised some shortcut keys. Space bar interferes with the tabbing/space bar method of operating the UI, so now using Pause key to Pause / Unpause the Force Simulation, instead of the Space bar.
    - Bug fix: Added Esc key handling for cancelling context menus (for real this time!).
    - Documentation: How to override default port number added to README.md.

--- AtomSpaceExplorer/src/app/shared/scrollable-table/scrollable-table.module.ts
+++ AtomSpaceExplorer/src/app/shared/scrollable-table/scrollable-table.module.ts
@@ -5,19 +5,23 @@ import { NgModule } from '@angular/core';
 
 import { ScrollDirective } from './directives/scroll.directive';
 import { ScrollableTableComponent } from './ui/scrollable-table.component';
-import {ScrollableTableService} from "./services/scrollable-table.service";
-import {CommonModule} from "@angular/common";
-import {MaterialModule} from "@angular/material";
+import { ScrollableTableService } from './services/scrollable-table.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatAutocompleteModule, MatInputModule,
+  MatFormFieldModule } from '@angular/material';
 
 @NgModule({
-  imports:[CommonModule,MaterialModule.forRoot(),],
+  imports: [ CommonModule, ],
   declarations: [
     ScrollDirective,
     ScrollableTableComponent
   ],
-  providers:[ScrollableTableService],
+  providers: [ ScrollableTableService ],
   exports: [
     ScrollableTableComponent
   ]
 })
-export class ScrollableTableModule{}
+
+export class ScrollableTableModule { }
